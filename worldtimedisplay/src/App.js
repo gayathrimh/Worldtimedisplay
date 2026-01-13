724c5df7411d8c1ec20ebdb7059d8b7de58a22e4
Gayathri <gayathrimh11@gmail.com>
2026-01-13 23:52:31 +0530

    Add worldtimedisplay and worldtimedisplay1 folders

---
+++ worldtimedisplay/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Page1 from './pages/Page1';
+
+function App() {
+  return (
+    <>
+    <p>My React App</p>
+    <Page1 />
+    </>
+  );
+}
+
+export default App;
---
+++ worldtimedisplay/src/pages/Page2.jsx
@@ -0,0 +1,27 @@
+import { useState,useEffect } from 'react';
+
+
+export default function Page2({city}){
+    const [time,setTime]=useState(new Date());
+
+    function updateTime(){
+        setTime(new Date());
+    }
+    useEffect(function setupInterval(){
+        const timer=setInterval(updateTime,1000);
+
+        return function cleanup(){
+            clearInterval(timer);
+        };
+    },[]);
+
+    const formattedTime=time.toLocaleTimeString('en-US',{
+        timeZone:city.timezone
+    });
+
+    return(
+        <div>
+            <p>{city.name}:{formattedTime}</p>
+        </div>
+    );
+}
